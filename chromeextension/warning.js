b102f1de62df5fd9799e64554c1128a5d1280d06
Yamini Kona <118597911+Yamini783@users.noreply.github.com>
2025-04-11 10:16:03 +0530

    Add files via upload

---
+++ chromeextension/warning.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("goBack").addEventListener("click", function () {
+        window.location.href = "https://www.google.com";
+    });
+});
